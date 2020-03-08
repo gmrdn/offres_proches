e811456bb4c609bf5f7d609aacfe5aeea6d86ba7
Guillaume R <guillaume.rondon@gmail.com>
2020-03-09 00:28:28 +0100

    stream csv et filtrage par distance

--- lib/offres_proches/filter_jobs.ex
+++ lib/offres_proches/filter_jobs.ex
@@ -30,4 +30,23 @@ defmodule OffresProches.FilterJobs do
   def is_in_radius({lat1, lon1}, {lat2, lon2}, radius) do
     calc_distance_km({lat1, lon1}, {lat2, lon2}) - radius <= 0
   end
+
+  def stream_csv(file) do
+    file
+    |> File.stream!()
+    |> CSV.decode(separator: ?,, headers: true)
+  end
+
+  def filter_datastream_by_location_and_radius(data, {lat, lon}, radius) do
+    data
+    |> Stream.filter(fn x ->
+      is_in_radius(
+        {String.to_float(Map.get(x, "office_latitude")),
+         String.to_float(Map.get(x, "office_longitude"))},
+        {lat, lon},
+        radius
+      )
+    end)
+    |> Enum.take(3)
+  end
 end
--- test/filter_jobs_test.exs
+++ test/filter_jobs_test.exs
@@ -23,4 +23,42 @@ defmodule OffresProches.FilterJobsTest do
   test "Should know that Paris is not in the radius of Marseille plus 30 km" do
     assert FilterJobs.is_in_radius(@paris, @marseille, 30) == false
   end
+
+  test "Should filter a CSV file by radius" do
+    data = [
+      %{
+        "contract_type" => "INTERNSHIP",
+        "name" =>
+          "[Louis Vuitton Germany] Praktikant (m/w) im Bereich Digital Retail (E-Commerce)",
+        "office_latitude" => "48.1392154",
+        "office_longitude" => "11.5781413",
+        "profession_id" => "7"
+      },
+      %{
+        "contract_type" => "INTERNSHIP",
+        "name" => "Bras droit de la fondatrice",
+        "office_latitude" => "48.885247",
+        "office_longitude" => "2.3566441",
+        "profession_id" => "5"
+      },
+      %{
+        "contract_type" => "FULL_TIME",
+        "name" => "[Louis Vuitton North America] Team Manager, RTW - NYC",
+        "office_latitude" => "40.7630463",
+        "office_longitude" => "-73.973527",
+        "profession_id" => "31"
+      }
+    ]
+
+    assert FilterJobs.filter_datastream_by_location_and_radius(data, @paris, 30) ==
+             [
+               %{
+                 "contract_type" => "INTERNSHIP",
+                 "name" => "Bras droit de la fondatrice",
+                 "office_latitude" => "48.885247",
+                 "office_longitude" => "2.3566441",
+                 "profession_id" => "5"
+               }
+             ]
+  end
 end
